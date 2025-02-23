ab84c3549b4f8843692ead269823e4cbda988e9d
Aleksander Spro <abspro@gmail.com>
2025-02-23 13:42:05 +0100

    Script syntax

--- plugins/victuals/turkey.lua
+++ plugins/victuals/turkey.lua
@@ -1,13 +1,13 @@
-local mq = require('mq')
-local logger = require('knightlinc/Write')
-local mqUtils = require('utils/mqhelpers')
-local binder = require('application/binder')
+local mq             = require('mq')
+local logger         = require('knightlinc/Write')
+local mqUtils        = require('utils/mqhelpers')
+local binder         = require('application/binder')
 
-local foodItemClicky  = "Endless Turkeys"
-local foodItem  = "Cooked Turkey"
-local maxFoodCount  = 5
+local foodItemClicky = "Endless Turkeys"
+local foodItem       = "Cooked Turkey"
+local maxFoodCount   = 5
 
-local function DoSummon (itemName)
+local function DoSummon(itemName)
   mq.cmdf('/nomodkey /itemnotify "%s" rightmouseup', itemName)
   mq.delay("3s", function() return mq.TLO.Cursor.ID() and mq.TLO.Cursor.ID() > 0 end)
   if mq.TLO.Cursor.ID() then
@@ -20,16 +20,16 @@ end
 local function execute()
   logger.Info('Start [SummonFood] ==> %s of %s.', maxFoodCount, foodItem)
 
-  if mq.TLO.FindItemCount('='..foodItemClicky)() < 1 then
+  if mq.TLO.FindItemCount('=' .. foodItemClicky)() < 1 then
     logger.Fatal('Missing item/spell <%s>', foodItemClicky)
   end
 
   mqUtils.ClearCursor()
-  while mq.TLO.FindItemCount('='..foodItem)() < maxFoodCount do
-    if mq.TLO.FindItem('='..foodItemClicky).TimerReady() == 0 then
-      logger.Info('Summoning: %s =>  %s/%s', foodItem, mq.TLO.FindItemCount('='..foodItem)()+1, maxFoodCount)
+  while mq.TLO.FindItemCount('=' .. foodItem)() < maxFoodCount do
+    if mq.TLO.FindItem('=' .. foodItemClicky).TimerReady() == 0 then
+      logger.Info('Summoning: %s =>  %s/%s', foodItem, mq.TLO.FindItemCount('=' .. foodItem)() + 1, maxFoodCount)
       DoSummon(foodItemClicky)
-      mq.delay(mq.TLO.FindItem('='..foodItemClicky).TimerReady() + 500)
+      mq.delay(mq.TLO.FindItem('=' .. foodItemClicky).TimerReady() + 500)
     end
 
     mqUtils.ClearCursor()
@@ -45,7 +45,7 @@ local function create(commandQueue)
     commandQueue.Enqueue(function() execute() end)
   end
 
-   mq.bind("/food", createCommand)
+  mq.bind("/food", createCommand)
 end
 
 return create
