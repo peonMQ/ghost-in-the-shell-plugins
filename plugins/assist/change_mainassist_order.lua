37a3a51366b026a232dc01e2c4d210fbecdd384b
Aleksander Spro <abspro@gmail.com>
2025-02-23 13:41:46 +0100

    Add scripts for temporarily change MT or MA list

---
+++ plugins/assist/change_mainassist_order.lua
@@ -0,0 +1,62 @@
+local mq = require("mq")
+local logger = require('knightlinc/Write')
+local broadCastInterfaceFactory = require("broadcast/broadcastinterface")
+local luaUtils = require('utils/lua-table')
+local assist = require('core/assist')
+local binder = require('application/binder')
+local settings = require('settings/settings')
+local bci = broadCastInterfaceFactory('ACTOR')
+
+---@param input string
+---@param sep string
+---@return string[]
+local function splitSet(input, sep)
+  if sep == nil then
+    sep = "|"
+  end
+
+  local t = {}
+  for str in string.gmatch(input, "([^" .. sep .. "]+)") do
+    table.insert(t, str)
+  end
+
+  return t
+end
+
+---@param new_ma_order_str string
+local function execute(new_ma_order_str)
+  local ma_order = splitSet(new_ma_order_str, ',')
+  local current_bots = bci.ConnectedClients()
+  local new_ma_order = {}
+  for _, maintank in ipairs(ma_order) do
+    if luaUtils.ContainsValue(current_bots, maintank) then
+      table.insert(new_ma_order, maintank)
+    end
+  end
+
+  if not next(new_ma_order) then
+    return
+  end
+
+  settings.assist.tanks = new_ma_order;
+end
+
+local function create(commandQueue)
+  logger.Info("Creating bind for '/maorder'.")
+  local function createCommand(new_ma_order)
+    if not new_ma_order then
+      return
+    end
+
+    if assist.IsOrchestrator() then
+      bci.ExecuteAllCommand("/maorder " .. new_ma_order)
+    end
+
+    commandQueue.Enqueue(function() execute(new_ma_order) end)
+  end
+
+  binder.Bind("/maorder", createCommand, "Changes MA order with given comma separated list 'ma_order'.",
+    'Fluffy,Humpty,Dumpty')
+end
+
+return create
---
+++ plugins/assist/change_maintank_order.lua
@@ -0,0 +1,62 @@
+local mq = require("mq")
+local logger = require('knightlinc/Write')
+local broadCastInterfaceFactory = require("broadcast/broadcastinterface")
+local luaUtils = require('utils/lua-table')
+local assist = require('core/assist')
+local binder = require('application/binder')
+local settings = require('settings/settings')
+local bci = broadCastInterfaceFactory('ACTOR')
+
+---@param input string
+---@param sep string
+---@return string[]
+local function splitSet(input, sep)
+  if sep == nil then
+    sep = "|"
+  end
+
+  local t = {}
+  for str in string.gmatch(input, "([^" .. sep .. "]+)") do
+    table.insert(t, str)
+  end
+
+  return t
+end
+
+---@param new_mt_order_str string
+local function execute(new_mt_order_str)
+  local mt_order = splitSet(new_mt_order_str, ',')
+  local current_bots = bci.ConnectedClients()
+  local new_mt_order = {}
+  for _, maintank in ipairs(mt_order) do
+    if luaUtils.ContainsValue(current_bots, maintank) then
+      table.insert(new_mt_order, maintank)
+    end
+  end
+
+  if not next(new_mt_order) then
+    return
+  end
+
+  settings.assist.tanks = new_mt_order;
+end
+
+local function create(commandQueue)
+  logger.Info("Creating bind for '/mtorder'.")
+  local function createCommand(new_mt_order)
+    if not new_mt_order then
+      return
+    end
+
+    if assist.IsOrchestrator() then
+      bci.ExecuteAllCommand("/mtorder " .. new_mt_order)
+    end
+
+    commandQueue.Enqueue(function() execute(new_mt_order) end)
+  end
+
+  binder.Bind("/mtorder", createCommand, "Changes MT order with given comma separated list 'mt_order'.",
+    'Fluffy,Humpty,Dumpty')
+end
+
+return create
